958f1a0d093291e2322ac526aa7379899758b746
ESitarski <edward.sitarski@gmail.com>
2016-03-04 06:55:07 -0500

    Fixed up release problems.

---
+++ SprintMgr.iss
@@ -0,0 +1,34 @@
+; -- SprintMgr.iss --
+
+[Setup]
+#include "inno_setup.txt"
+ArchitecturesInstallIn64BitMode=x64
+DefaultDirName={pf}\SprintMgr
+DefaultGroupName=SprintMgr
+UninstallDisplayIcon={app}\SprintMgr.exe
+Compression=lzma2/ultra64
+SolidCompression=yes
+SourceDir=dist\SprintMgr
+OutputDir=..\..\install
+OutputBaseFilename=SprintMgr_Setup
+ChangesAssociations=yes
+
+[Registry]
+; Automatically configure SprintMgr to launch .smr files.
+Root: HKCR; Subkey: ".smr"; ValueType: string; ValueName: ""; ValueData: "SprintMgr"; Flags: uninsdeletevalue
+Root: HKCR; Subkey: "SprintMgr"; ValueType: string; ValueName: ""; ValueData: "SprintMgr Race File"; Flags: uninsdeletekey
+Root: HKCR; Subkey: "SprintMgr\DefaultIcon"; ValueType: string; ValueName: ""; ValueData: "{app}\SprintMgr.exe,0"
+Root: HKCR; Subkey: "SprintMgr\shell\open\command"; ValueType: string; ValueName: ""; ValueData: """{app}\SprintMgr.exe"" ""%1"""
+
+[Tasks] 
+Name: "desktopicon"; Description: "Create a &desktop icon"; 
+	
+[Files]
+Source: "*.*"; DestDir: "{app}"; Flags: recursesubdirs
+
+[Icons]
+Name: "{group}\SprintMgr"; Filename: "{app}\SprintMgr.exe"
+Name: "{userdesktop}\SprintMgr"; Filename: "{app}\SprintMgr.exe"; Tasks: desktopicon
+
+[Run]
+Filename: "{app}\SprintMgr.exe"; Description: "Launch SprintMgr"; Flags: nowait postinstall skipifsilent
--- SprintMgrSetup.py
+++ SprintMgrSetup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
-import py2exe
 import os
 import shutil
 import zipfile
 
 distDir = 'dist'
+
+googleDrive = r"c:\GoogleDrive\Downloads\Windows\CrossMgr"
+if not os.path.exists(googleDrive):
+	googleDrive = r"C:\Users\Edward Sitarski\Google Drive\Downloads\Windows\CrossMgr"
 
 # Cleanup existing dll, pyd and exe files.  The old ones may not be needed, so it is best to clean these up.
 for f in os.listdir(distDir):
@@ -13,14 +16,26 @@ for f in os.listdir(distDir):
 		print 'deleting:', fname
 		os.remove( fname )
 		
-setup( windows=
-			[
-				{
-					'script': 'SprintMgr.pyw',
-					'icon_resources': [(1, r'images\SprintMgr.ico')]
-				}
-			]
-	 )
+from Version import AppVerName
+def make_inno_version():
+	setup = {
+		'AppName':				AppVerName.split()[0],
+		'AppPublisher':			"Edward Sitarski",
+		'AppContact':			"Edward Sitarski",
+		'AppCopyright':			"Copyright (C) 2004-{} Edward Sitarski".format(datetime.date.today().year),
+		'AppVerName':			AppVerName,
+		'AppPublisherURL':		"http://www.sites.google.com/site/crossmgrsoftware/",
+		'AppUpdatesURL':		"http://www.sites.google.com/site/crossmgrsoftware/downloads/",
+		'VersionInfoVersion':	AppVerName.split()[1],
+	}
+	with open('inno_setup.txt', 'w') as f:
+		for k, v in setup.iteritems():
+			f.write( '{}={}\n'.format(k,v) )
+make_inno_version()
+
+cmd = '"' + inno + '" ' + 'CrossMgr.iss'
+print cmd
+subprocess.call( cmd, shell=True )
 
 # Copy additional dlls to distribution folder.
 wxHome = r'C:\Python27\Lib\site-packages\wx-2.8-msw-ansi\wx'
@@ -49,13 +64,14 @@ copyDir( 'images' )
 #copyDir( 'html' )
 
 # Create the installer
-inno = r'\Program Files\Inno Setup 5\ISCC.exe'
-# Find the drive it is installed on.
+inno = r'\Program Files (x86)\Inno Setup 5\ISCC.exe'
+# Find the drive inno is installed on.
 for drive in ['C', 'D']:
 	innoTest = drive + ':' + inno
 	if os.path.exists( innoTest ):
 		inno = innoTest
 		break
+
 cmd = '"' + inno + '" ' + 'SprintMgr.iss'
 print cmd
 os.system( cmd )
